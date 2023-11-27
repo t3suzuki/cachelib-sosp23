e0101625613a24fe6740e077b91c3fbec2e78250
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-11-27 19:50:56 +0000

    up

--- mybench/main.cpp
+++ mybench/main.cpp
@@ -27,23 +27,33 @@ int main(int argc, char *argv[]) {
 #define VAL_LEN (16)
   // warmup
   int64_t i;
+  int i_th;
+  int n_th = opts.n_thread;
+  uint64_t cnt = 0;
   printf("warmup...\n");
-  for (i=(1<<(opts.hashpower-1)); i>=0; i--) {
-    //printf("warmup... %d\n", i);
-    struct request req;
-    char dummy_val[VAL_LEN];
-    req.timestamp = 86400 * -2;
-    *(uint64_t *)req.key = (uint64_t)i;
-    req.key_len = 8;
-    req.val = dummy_val;
-    req.val_len = VAL_LEN;
-    req.op = op_set;
-    req.ttl = 2000000;
-    struct bench_data *bdata = &bench_data;
-    cache_go(bdata->cache, bdata->pool, &req, &bdata->n_get, &bdata->n_set,
-	     &bdata->n_del, &bdata->n_get_miss);
+  for (i=(1<<(opts.hashpower-1))/n_th; i>=0; i--) {
+    for (i_th=0; i_th<n_th; i_th++) {
+      //printf("warmup... %d\n", i);
+      struct request req;
+      char dummy_val[VAL_LEN];
+      req.timestamp = 86400 * -2;
+      if (n_th > 1) {
+	*(uint64_t *)req.key = (uint64_t)i + (i_th+1) * 1000000000ULL;
+      } else {
+	*(uint64_t *)req.key = (uint64_t)i;
+      }
+      req.key_len = 8;
+      req.val = dummy_val;
+      req.val_len = VAL_LEN;
+      req.op = op_set;
+      req.ttl = 200000000;
+      struct bench_data *bdata = &bench_data;
+      cache_go(bdata->cache, bdata->pool, &req, &bdata->n_get, &bdata->n_set,
+	       &bdata->n_del, &bdata->n_get_miss);
+      cnt++;
+    }
   }
-  printf("warmup done!\n");
+  printf("warmup done! %ld items inserted\n", cnt);
 #endif
 
   if (opts.n_thread == 1) {
